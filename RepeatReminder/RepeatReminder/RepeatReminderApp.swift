af9eed785bcc48458f18525a879b25f5f079e455
ibuibukiki <rocoroco523@gmail.com>
2023-11-05 23:08:59 +0900

    initial commit

---
+++ RepeatReminder/RepeatReminder/RepeatReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepeatReminderApp.swift
+//  RepeatReminder
+//
+//  Created by 吉田郁吹 on 2023/11/05.
+//
+
+import SwiftUI
+
+@main
+struct RepeatReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
