feb06407b4ff89256eb792a77dfe04aafd99c815
ibuibukiki <rocoroco523@gmail.com>
2023-11-13 23:19:35 +0900

    [update] ボタンを表示

--- RepeatReminder/RepeatReminder/Presentation/Views/TaskListView.swift
+++ RepeatReminder/RepeatReminder/Presentation/Views/TaskListView.swift
@@ -25,15 +25,50 @@ struct TaskListView: View {
                         Text("・レポート提出")
                             .font(.title3)
                     }.foregroundColor(Color("TextColor"))
-                        .padding()
-                        .frame(width:296,height:152,alignment:.topLeading)
-                        .background(Color("BackgroundColor"))
-                        .cornerRadius(10)
+                    .padding()
+                    .frame(width:296,height:152,alignment:.topLeading)
+                    .background(Color("BackgroundColor"))
+                    .cornerRadius(10)
                 }.frame(width:328,height:232,alignment:.top)
-                    .background(Color("MainColor"))
-                    .cornerRadius(15)
-                    .compositingGroup()
-                    .shadow(color:.gray,radius:5,x:0,y:10)
+                .background(Color("MainColor"))
+                .cornerRadius(15)
+                .compositingGroup()
+                .shadow(color:.gray,radius:5,x:0,y:10)
+                .padding(.bottom,24)
+                // ボタンを表示
+                HStack{
+                    Button(action:{
+                        print("tap setting button")
+                    }){
+                        Text("設 定")
+                            .fontWeight(.bold)
+                            .font(.title2)
+                            .foregroundColor(Color("ButtonColor"))
+                            .frame(width:144,height:56)
+                            .background(Color("BackgroundColor"))
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color("ButtonColor"), lineWidth: 4)
+                            )
+                            .compositingGroup()
+                            .shadow(color:.gray,radius:5,x:0,y:5)
+                    }
+                    .padding(.trailing,24)
+                    Button(action:{
+                        print("tap setting button")
+                    }){
+                        Text("追 加")
+                            .fontWeight(.bold)
+                            .font(.title2)
+                            .foregroundColor(Color("BackgroundColor"))
+                            .frame(width:144,height:56)
+                            .background(Color("ButtonColor"))
+                            .cornerRadius(10)
+                            .compositingGroup()
+                            .shadow(color:.gray,radius:5,x:0,y:5)
+                    }
+                }
             }
         }
     }
