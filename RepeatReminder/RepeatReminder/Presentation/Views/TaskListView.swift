fc2c45bde0b13e54ff23147c317b9e388bf64e8a
ibuibukiki <rocoroco523@gmail.com>
2023-11-13 23:18:23 +0900

    [update] 今日までのタスクを表示

--- RepeatReminder/RepeatReminder/Presentation/Views/TaskListView.swift
+++ RepeatReminder/RepeatReminder/Presentation/Views/TaskListView.swift
@@ -12,8 +12,29 @@ struct TaskListView: View {
         ZStack{
             Color("BackgroundColor")
                 .edgesIgnoringSafeArea(.all)
-            Text("Color test")
-                .foregroundColor(Color("TextColor"))
+            // 今日までのタスクを表示
+            VStack{
+                VStack{
+                    Text("今日までのタスク")
+                        .foregroundColor(Color("BackgroundColor"))
+                        .fontWeight(.bold)
+                        .font(.title2)
+                        .padding(.top,20)
+                        .padding(.bottom,8)
+                    VStack{
+                        Text("・レポート提出")
+                            .font(.title3)
+                    }.foregroundColor(Color("TextColor"))
+                        .padding()
+                        .frame(width:296,height:152,alignment:.topLeading)
+                        .background(Color("BackgroundColor"))
+                        .cornerRadius(10)
+                }.frame(width:328,height:232,alignment:.top)
+                    .background(Color("MainColor"))
+                    .cornerRadius(15)
+                    .compositingGroup()
+                    .shadow(color:.gray,radius:5,x:0,y:10)
+            }
         }
     }
 }
