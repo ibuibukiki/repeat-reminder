76355508ac9a921b1d23d1f28e08e506f92a94cd
ibuibukiki <rocoroco523@gmail.com>
2023-11-14 14:53:42 +0900

    [update] ボタンを表示

--- RepeatReminder/RepeatReminder/Presentation/Views/TaskAddEditView.swift
+++ RepeatReminder/RepeatReminder/Presentation/Views/TaskAddEditView.swift
@@ -17,6 +17,7 @@ struct TaskAddEditView: View {
                     .foregroundColor(Color("MainColor"))
                     .fontWeight(.bold)
                     .font(.title)
+                    .frame(width:328,height:56,alignment:.leading)
                 VStack(alignment:.leading,spacing:24){
                     // タスクの情報を表示・入力
                     Text("基本設定")
@@ -62,10 +63,59 @@ struct TaskAddEditView: View {
                     .padding(.bottom,48)
                     .padding(.leading,24)
                     .frame(width:320,height:400,alignment:.leading)
+                    .background(Color("BackgroundColor"))
+                    .cornerRadius(20)
                     .overlay(
                         RoundedRectangle(cornerRadius: 20)
                             .stroke(Color("MainColor"), lineWidth: 4)
                     )
+                    .compositingGroup()
+                    .shadow(color:.gray,radius:5,x:0,y:10)
+                    .padding(.bottom,24)
+                Spacer()
+                // ボタンを表示
+                HStack(spacing:24){
+                    Button(action:{
+                        print("tap cancel button")
+                    }){
+                        Text("キャンセル")
+                            .fontWeight(.bold)
+                            .font(.title2)
+                            .foregroundColor(Color("MainColor"))
+                            .frame(width:152,height:56)
+                            .background(Color("BackgroundColor"))
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color("MainColor"), lineWidth: 4)
+                            )
+                            .compositingGroup()
+                            .shadow(color:.gray,radius:5,x:0,y:5)
+                    }
+                    Button(action:{
+                        print("tap add button")
+                    }){
+                        Text("追 加")
+                            .fontWeight(.bold)
+                            .font(.title2)
+                            .foregroundColor(Color("BackgroundColor"))
+                            .frame(width:152,height:56)
+                            .background(Color("ButtonColor"))
+                            .cornerRadius(10)
+                            .compositingGroup()
+                            .shadow(color:.gray,radius:5,x:0,y:5)
+                    }
+                }.padding(.bottom,24)
+                // 削除ボタン(編集時のみ表示)
+                Button(action:{
+                    print("tap delete button")
+                }){
+                    Text("このタスクを削除")
+                        .fontWeight(.bold)
+                        .font(.title2)
+                        .foregroundColor(Color("ButtonColor"))
+                        .frame(width:200,height:40)
+                }.padding(.bottom,32)
             }.padding(.top,40)
         }
     }
