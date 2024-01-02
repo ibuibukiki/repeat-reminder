a409c174298d809f413454cef5c8b01da7d41ee4
ibuibukiki <rocoroco523@gmail.com>
2024-01-02 16:59:10 +0900

    [update] 通知の登録、削除機能を実装

--- RepeatReminder/RepeatReminder/Domain/Usecase/NotificationManager.swift
+++ RepeatReminder/RepeatReminder/Domain/Usecase/NotificationManager.swift
@@ -10,17 +10,32 @@ import UserNotifications
 
 final class NotificationManager {
     
-    func sendNotification() {
-        let content = UNMutableNotificationContent()
-        content.title = "Notification Title"
-        content.body = "Local Notification Test"
-        content.sound = UNNotificationSound.default
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        let request = UNNotificationRequest(identifier: "notification01", content: content, trigger: trigger)
-        UNUserNotificationCenter.current().add(request) { (error: Error?) in
-            if error != nil {
-                print("send Notification Error: \(String(describing: error))")
+    /// 通知の送信設定 (以前設定した通知の変更も可能)
+    /// - Parameters:
+    ///   - id: 送信する通知IDの配列
+    ///   - title: 通知に表示するテキストの配列
+    ///   - date: 送信する日時
+    func sendNotification(id: [String], title: [String], date:[DateComponents]) {
+        for i in 0 ..< id.count {
+            let content = UNMutableNotificationContent()
+            content.body = title[i]
+            content.sound = UNNotificationSound.default
+        
+            let trigger = UNCalendarNotificationTrigger(dateMatching: date[i], repeats: false)
+            
+            let request = UNNotificationRequest(identifier: id[i], content: content, trigger: trigger)
+            UNUserNotificationCenter.current().add(request) { (error: Error?) in
+                if error != nil {
+                    print("send Notification Error: \(String(describing: error))")
+                }
             }
         }
+    }
+    
+    /// 通知設定の削除
+    /// - Parameters:
+    ///   - id: 削除する通知IDの配列
+    func removeNotification(id: [String]) {
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: id)
     }
 }
